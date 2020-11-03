9d23ebe9d602cac5c7ea9302aad4bed4c85488e5
HinataKikuchi <haiao4041@gmail.com>
2020-11-03 23:24:46 +0900

    added memcmp

--- libft/main.c
+++ libft/main.c
@@ -17,12 +17,26 @@ int main(void)
 //    printf("after：%s\n",str);
 //    return 0;
 
-    char str[] = "abcdef\0ghij";    /* 途中に空文字のある文字列 */
-    char *p;
-    
-    p = ft_memchr(str, 'h', 12);
-    printf("検索文字は文字列の%d番目\n",p - str);
-    
+    //char str[] = "abcdef\0ghij";
+    //char *p;
+    //
+    //p = ft_memchr(str, 'h', 12);
+    //printf("number = %ld\n",p - str);
+    //
+
+    char buf1[] = "\0abc\0de";      /* 途中に空文字のある文字列 */
+        char buf2[] = "\0abc\0de";
+        char buf3[] = "\0abcdef";
+        
+        if (ft_memcmp(buf1, buf2, 7) == 0)
+                printf("buf1 = buf2\n");
+        else
+                printf("buf1 != buf2\n");
+        if (ft_memcmp(buf1, buf3, 7) == 0)
+                printf("buf1 = buf3\n");
+        else
+                printf("buf1 != buf3\n");
+
     return 0;
 
 }
