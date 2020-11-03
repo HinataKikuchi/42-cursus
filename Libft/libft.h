1767959aea8670bf95212f113feaddfd764876c5
HinataKikuchi <haiao4041@gmail.com>
2020-11-03 16:59:54 +0900

    added functions and fixed some

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 15:15:01 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/03 12:52:41 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/03 16:56:46 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 void	*ft_memset(void *bun, int ch, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *buf1, const void *buf2, size_t n);
+void	*ft_memmove(void *buf1, const void *buf2, size_t n);
+
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
--- Libft/main.c
+++ Libft/main.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 int main(void)
 {
-	char str[] = "0123456789";
-	ft_bzero(str+2, 3);
-	printf("%s\n",str);
+	char str1[] = "0123456789";
+	char str2[] = "6789";
+	ft_memcpy(str1+2,str2,3);
+	printf("%s\n",str1);
 	return 0;	
 }
