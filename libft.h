971d39508ae901b55e7e5470ab04e91574c92ca1
HinataKikuchi <haiao4041@gmail.com>
2020-11-03 22:14:50 +0900

    all

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 15:15:01 by hkikuchi          #+#    #+#             */
+/*   Updated: 2020/11/03 16:56:46 by hkikuchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdio.h>
+
+void	*ft_memset(void *bun, int ch, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *buf1, const void *buf2, size_t n);
+void	*ft_memmove(void *buf1, const void *buf2, size_t n);
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+void	ft_putchar_fd(char c, int fd);
+
+#endif
