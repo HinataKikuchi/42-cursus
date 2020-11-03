27e4c104cef7bbf50c4798776750c8841027375e
HinataKikuchi <haiao4041@gmail.com>
2020-11-03 18:32:11 +0900

    made memmove

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 12:52:49 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/03 16:49:49 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/03 18:29:24 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void	*ft_memcpy(void *buf1, const void *buf2, size_t n)
 {
 	unsigned int i;
-	char *b1;
-	char *b2;
-	
-	b1=(char *)buf1;
-	b2=(char *)buf2;
-	i=0; 
+	unsigned char *b1;
+	unsigned char *b2;
+
+	b1=(unsigned char *)buf1;
+	b2=(unsigned char *)buf2;
+	i=0;
 	while(i < n)
 	{
 		*(b1+i)=*(b2+i);
 		i++;
-	}
+	}	
 	return b1;
 }
--- Libft/main.c
+++ Libft/main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 int main(void)
 {
-	char str1[] = "0123456789";
-	char str2[] = "6789";
-	ft_memcpy(str1+2,str2,3);
-	printf("%s\n",str1);
-	return 0;	
+//	char str1[] = "0123456789";
+//	char str2[] = "6789";
+//	ft_memcpy(str1+2,str2,3);
+//	printf("%s\n",str1);
+//	return 0;
+
+	char str[] = "abcdefghijklmnopqrstu";
+        
+    printf("before;%s\n",str);
+    
+    ft_memmove(str+5, str, 10);
+    printf("after：%s\n",str);
+    return 0;
 }
