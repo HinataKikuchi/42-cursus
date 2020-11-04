50f0ecab572a0734fe8271205fb72a96bcc06718
HinataKikuchi <haiao4041@gmail.com>
2020-11-04 19:30:40 +0900

    fixed ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 18:51:40 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/04 19:05:25 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/04 19:30:13 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ size_t	ft_strlcat(char *buf1, const char *buf2, size_t n)
 	unsigned int i;
 
 	i = ft_strlen(buf2);
-	buf1 += n;
+	buf1 += n - 1;
 	while(i != 0)
 	{
 		*buf1++ = *buf2++;
 		i--;
 	}
-	return (ft_strlen(buf2) + n);
+	*buf1++ = '\0';
+	return (ft_strlen(buf2) + n - 1);
 }
