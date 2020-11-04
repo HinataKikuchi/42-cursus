d00a46065070e49e29335776e38ffe3993cc8eac
HinataKikuchi <haiao4041@gmail.com>
2020-11-04 11:31:00 +0900

    added memccpy

---
+++ libft/ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 10:55:02 by hkikuchi          #+#    #+#             */
+/*   Updated: 2020/11/04 11:29:59 by hkikuchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *buf1, const void *buf2, int c, size_t n)
+{
+	unsigned int	i;
+	unsigned char	*b1;
+	unsigned char	*b2;
+
+	i = 0;
+	b1 = buf1;
+	b2 = (unsigned char *)buf2;
+	while (i < n)
+	{
+		*(b1 + i) = *(b2 + i);
+		if (*(b1 + i) == (char)c)
+			return (b1 + i);
+		i++;
+	}
+	return (NULL);
+}
--- libft/main.c
+++ libft/main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "libft.h"
 
 int main(void)
 {
 //	char str1[] = "0123456789";
 //	char str2[] = "6789";
+//        char str3[] = "0123456789";
 //	ft_memcpy(str1+2,str2,3);
 //	printf("%s\n",str1);
+//        ft_memcpy(str3+2,str2,3);
+//	printf("%s\n",str3);
 //	return 0;
+
+
+//        char str1[] = "0123456789";
+//        char str2[] = "6789";
+//        ft_memccpy(str1+2,str2, '4',3);
+//        printf("%s\n",str1);
+//        memccpy(str3+2,str2, '4',3);
+//        printf("%s\n",str3);
+//        return 0;
+//
 
 //	char str[] = "abcdefghijklmnopqrstu";
 //        
@@ -24,19 +38,19 @@ int main(void)
     //printf("number = %ld\n",p - str);
     //
 
-    char buf1[] = "\0abc\0de";      /* 途中に空文字のある文字列 */
-        char buf2[] = "\0abc\0de";
-        char buf3[] = "\0abcdef";
-        
-        if (ft_memcmp(buf1, buf2, 7) == 0)
-                printf("buf1 = buf2\n");
-        else
-                printf("buf1 != buf2\n");
-        if (ft_memcmp(buf1, buf3, 7) == 0)
-                printf("buf1 = buf3\n");
-        else
-                printf("buf1 != buf3\n");
-
+//    char buf1[] = "\0abc\0de";      /* 途中に空文字のある文字列 */
+//        char buf2[] = "\0abc\0de";
+//        char buf3[] = "\0abcdef";
+//        
+//        if (ft_memcmp(buf1, buf2, 7) == 0)
+//                printf("buf1 = buf2\n");
+//        else
+//                printf("buf1 != buf2\n");
+//        if (ft_memcmp(buf1, buf3, 7) == 0)
+//                printf("buf1 = buf3\n");
+//        else
+//                printf("buf1 != buf3\n");
+//
     return 0;
 
 }
