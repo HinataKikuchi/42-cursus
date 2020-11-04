553272bcdf63d82b3b1bde19000c29765f5d4e36
HinataKikuchi <haiao4041@gmail.com>
2020-11-04 17:01:37 +0900

    added files

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,22 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 16:11:33 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/04 15:20:01 by hkikuchi         ###   ########.fr       */
+/*   Created: 2020/11/04 12:07:20 by hkikuchi          #+#    #+#             */
+/*   Updated: 2020/11/04 16:42:18 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+size_t ft_strlcpy(char *buf1, const char *buf2, size_t n)
 {
-	char	*tmp;
+	unsigned int	i;
+	 char	*b1;
+	unsigned char	*b2;
 
-	tmp = s;
-	while (0 < n--)
-		*tmp++ = '\0';
+	i = n;
+	b1 = buf1;
+	b2 = (unsigned char *)buf2;
+	if (i != 0)
+	{
+		while (--i != 0)
+		{
+			if ((*b1++ = *b2++) == '\0')
+				break;
+		}
+	}
+	else if (i == 0)
+	{
+		if (n != 0)
+			*b1='\0';
+	}
+	return (ft_strlen(buf2));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 15:15:01 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/04 10:54:50 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/04 12:07:16 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memccpy(void *buf1, const void *buf2, int c, size_t n);
 void	*ft_memmove(void *buf1, const void *buf2, size_t n);
 void	*ft_memchr(const void *buf, int ch, size_t n);
 int		ft_memcmp(const void *buf1, const void *buf2, size_t n);
+size_t	ft_strlen(const char *s);
+size_t  ft_strlcpy(char *buf1, const char *buf2, size_t n);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
