34a4498d8c19cdfbb05be4d2067a603d839e9193
HinataKikuchi <haiao4041@gmail.com>
2020-11-06 17:50:31 +0900

    added isascii

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,15 +6,17 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 17:24:05 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/06 17:26:06 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/06 17:31:43 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-	if(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'))
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
 		return (1);
-	return 0;
+	else if('0' <= c && c <= '9')
+		return (1);
+	return (0);
 }
