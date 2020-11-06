7fa5733d54f3723d5952c46d7894d2687e378b59
HinataKikuchi <haiao4041@gmail.com>
2020-11-06 16:50:53 +0900

    fixed atoi with norminette

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 13:03:06 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/06 16:42:24 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/06 16:50:23 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ int	ft_atoi(const char *nptr)
 	int sign;
 
 	sign = 1;
-	ans = 0;	
-	while (('\t' <= *nptr && *nptr  <= '\r')|| *nptr == ' ')
+	ans = 0;
+	while (('\t' <= *nptr && *nptr <= '\r') || *nptr == ' ')
 		nptr++;
 	if (*nptr == '-')
 	{
 		sign = -1;
 		nptr++;
 	}
-	else if(*nptr == '+')
+	else if (*nptr == '+')
 		nptr++;
-	while(*nptr != '\0' && ('0' <= *nptr && *nptr <= '9'))
+	while (*nptr != '\0' && ('0' <= *nptr && *nptr <= '9'))
 	{
 		ans = ans * 10;
 		ans += (int)*nptr - '0';
 		nptr++;
 	}
-	return ans * sign;
+	return (ans * sign);
 }
