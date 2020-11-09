a8d49f6c7dabae1e0bc8e0b9c0f916cdc8722d6f
HinataKikuchi <haiao4041@gmail.com>
2020-11-09 22:29:00 +0900

    added strtrim but not completed

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,26 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/05 15:42:59 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/09 16:07:28 by hkikuchi         ###   ########.fr       */
+/*   Created: 2020/11/09 12:48:30 by hkikuchi          #+#    #+#             */
+/*   Updated: 2020/11/09 22:28:43 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	while (*s!= '\0')
+	char	*ans;
+	int		i;
+
+	ans = (char *)ft_calloc((ft_strlen(s1) + 1),sizeof(char));
+	if (ans == NULL)
+		return (NULL);
+	i = 0;
+	while(*(set + i) != '\0')
 	{
-		if (*s == (char)c)
-			return (char *)(s);
-		s++;
+		printf("*(set + i) = %c\n *s1 = %c\n", *(set+i), *s1);
+		if((*(set + i) - *s1) == 0)
+			i++;
+		if(i == 0)
+			break ;
+		s1++;
 	}
-	if (*s == (char)c)
-		return (char *)(s);
-	return (NULL);
+	
+	ans = (char *)s1;
+
+	return (ans);
 }
