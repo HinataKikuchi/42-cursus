46bddf8febf5f297b6896ae89cc47d6869e31073
HinataKikuchi <haiao4041@gmail.com>
2020-11-10 14:27:10 +0900

    add putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,15 +6,33 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 13:09:23 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/10 13:20:34 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/10 14:26:56 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	printf("%d",INT_MIN);
+	unsigned int	i;
+	char			c;
+	int				tmp1;
+	int				tmp2;
+	i = n;
+	if(n < 0)
+	{
+		i = (-1) * i;
+		write(fd, "-", 1);
+	}
+	tmp1 = i / 10;
+	tmp2 = i % 10;
+	c = tmp2 + '0';
+	if(tmp2)
+		ft_putnbr_fd(tmp1, fd);
+	else
+		return ;
+	ft_putchar_fd(c, fd);
 }
