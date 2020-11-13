3dd2500532fc400e456b9ce9219dc9c98f8aed54
HinataKikuchi <haiao4041@gmail.com>
2020-11-13 13:48:16 +0900

    fixed ft_atoi to deal with long size

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,19 +6,32 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 13:03:06 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/12 23:14:29 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/13 13:44:43 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
+#include <stdio.h>
 // #include "main.c"
+// #include "ft_strlen.c"
+// #include "ft_strncmp.c"
+
+int	judge(const char *n)
+{
+	if (ft_strncmp(n,"9223372036854775807",18) >= 0)
+		return (1);
+	else if (ft_strlen(n) > 19)
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *nptr)
 {
 	int ans;
 	int sign;
 
+	if(judge(nptr))
+		return (-1);
 	sign = 1;
 	ans = 0;
 	while (('\t' <= *nptr && *nptr <= '\r') || *nptr == ' ')
@@ -37,14 +50,4 @@ int	ft_atoi(const char *nptr)
 		nptr++;
 	}
 	return (ans * sign);
-}
-
-int	judge(const char *n)
-{
-	
-}
-
-int ft_count(const char *n)
-{
-	
 }
