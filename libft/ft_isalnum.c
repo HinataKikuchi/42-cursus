cefd5c823ccb8ecea316d0b0a4072fa43074bfcd
HinataKikuchi <haiao4041@gmail.com>
2020-11-16 12:37:25 +0900

    all

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 17:24:05 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/06 18:10:08 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/16 12:37:15 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 int	ft_isalnum(int c)
 {
-	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
-		return (1);
-	else if ('0' <= c && c <= '9')
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
