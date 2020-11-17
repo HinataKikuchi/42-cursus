10b0aab5473bd85967f0493db0e944eb54f30672
HinataKikuchi <haiao4041@gmail.com>
2020-11-17 18:58:49 +0900

    FUCK MY BRAIN OH MY

--- libft/test.c
+++ libft/test.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
-#include <string.h>
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
-int main(void)
+int main( void )
 {
-//	printf("strlcpy: %u\n",strlcpy("","",0));
-printf("ft_strlcpy: %ld\n",ft_strlcpy("", "", 0));
-	return 0;
+    char *s2 = "";
+    char *s3 = "";
+    printf("man=%lu\n",strlcat(s2,"",0));
+    printf("man=%s\n",s2);
+    write(1,s2,1);
+    write(1,"\n",1);
+    printf("ft=%lu\n",ft_strlcat(s3,"",0));
+    printf("ft=%s\n",s3);
+    write(1,s3,1);
+    write(1,"\n",1);
 }
