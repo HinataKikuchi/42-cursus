3570032c4df469a651e71eef8d1e99f6400ea49c
HinataKikuchi <haiao4041@gmail.com>
2020-11-18 16:29:13 +0900

    atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,45 +6,44 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 19:53:22 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/13 19:53:24 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/18 16:16:44 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	judge(const char *n)
+void	ft_skip_blank(const char *nptr)
 {
-	if (ft_strncmp(n, "9223372036854775807", 18) >= 0)
-		return (1);
-	else if (ft_strlen(n) > 19)
-		return (1);
-	return (0);
-}
-
-int	ft_atoi(const char *nptr)
-{
-	int ans;
-	int sign;
-
-	if (judge(nptr))
-		return (-1);
-	sign = 1;
-	ans = 0;
 	while (('\t' <= *nptr && *nptr <= '\r') || *nptr == ' ')
 		nptr++;
-	if (*nptr == '-')
+}
+
+int		ft_atoi(const char *nptr)
+{
+	unsigned long	ans;
+	int				sign;
+	char			*tmp;
+
+	sign = 1;
+	ans = 0;
+	ft_skip_blank(nptr);
+	if (*nptr == '-' || *nptr == '+')
 	{
-		sign = -1;
+		if (*nptr == '-')
+			sign = -1;
 		nptr++;
 	}
-	else if (*nptr == '+')
-		nptr++;
-	while (*nptr != '\0' && ('0' <= *nptr && *nptr <= '9'))
+	tmp = (char *)nptr;
+	while (*nptr != '\0' && ft_isdigit(*nptr))
 	{
 		ans = ans * 10;
 		ans += (int)*nptr - '0';
 		nptr++;
+		if (((nptr - tmp) > 19 || ans >= 9223372036854775807) && sign == 1)
+			return (-1);
+		else if (ans > 9223372036854775807)
+			return (0);
 	}
-	return (ans * sign);
+	return ((int)(ans * sign));
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 12:07:20 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/11/17 18:43:07 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/11/18 12:26:21 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ size_t	ft_strlcpy(char *buf1, const char *buf2, size_t n)
 	i = n;
 	b1 = (unsigned char *)buf1;
 	b2 = (unsigned char *)buf2;
-	if (ft_strlen(buf1) == 0)
+	if (sizeof(buf1) == 0 || n == 0)
 		return (ft_strlen(buf2));
 	if (i != 0)
 	{
