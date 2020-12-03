60adb8214a092736026acae6689c33ead7909fe2
HinataKikuchi <haiao4041@gmail.com>
2020-12-03 13:18:52 +0900

    added array

--- mini_ls/ft_mini_ls.c
+++ mini_ls/ft_mini_ls.c
@@ -6,35 +6,81 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 15:36:52 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/12/02 18:41:28 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/12/03 12:27:05 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mini_ls.h"
 #include "../libft/libft.h"
+#include <stdio.h>
+
+void ft_swap(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int *ft_bubble(int *ans, int size)
+{
+
+}
+
+int ft_count_file(const char *pathname)
+{
+	DIR *dir;
+	struct dirent *dp;
+	int res;
+
+	if(!(dir = opendir(pathname)))
+		return (-1);
+	if (!(dp = readdir(dir)))
+		return (-1);
+	res = 0;
+	while (dp)
+	{
+		if (*dp->d_name != '.')
+			res++;
+		dp = readdir(dir);
+	}
+	closedir(dir);
+	return (res);
+}
 
 void ft_mini_ls(void)
 {
 	DIR *dir;
-	char *path;
 	struct dirent *dp;
-	struct stat *buf;
-	int status;
+	struct stat buf;
+	int *array;
+	int i;
 
-	path = "./";
-	dir = opendir(path);
-	dp = readdir(dir);
+	if ((i = ft_count_file("./")) == -1)
+	{
+		perror("CANNOT OPEN OR READ DIR");
+		return ;
+	}
+	if(!(dir = opendir("./")))
+		return ;
+	if(!(dp = readdir(dir)))
+		return ;
+	array = malloc(sizeof(int) * i);
 	while (dp)
 	{
 		if (*dp->d_name != '.')
 		{
-			ft_putstr_fd(dp->d_name,1);
-			write(1,"\n",1);
+			if(!(lstat(dp->d_name,&buf)) && --i>=0)
+			{
+				array[i]=buf.st_mtime;
+				printf("array[%d] = %d\n",i, array[i]);
+			}
+			else
+				perror("STAT ERROR");
 		}
 		dp = readdir(dir);
 	}
-
-
+	closedir(dir);
 }
 
 int main (void)
--- mini_ls/ft_mini_ls.h
+++ mini_ls/ft_mini_ls.h
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 15:36:49 by hkikuchi          #+#    #+#             */
-/*   Updated: 2020/12/02 18:38:47 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2020/12/03 12:01:27 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define MINI_LS
 # include <dirent.h>
 # include <sys/stat.h>
+# include <stdio.h>
 # include <unistd.h>
 # include "../libft/libft.h"
+
 
 
 
