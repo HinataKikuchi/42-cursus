a93996de07fbdcf9b533f996020f4e29faf150c3
HinataKikuchi <haiao4041@gmail.com>
2020-12-27 15:34:29 +0900

    added ex04

--- hello_node/ex04/asyncio.js
+++ hello_node/ex04/asyncio.js
@@ -1,3 +1,17 @@
-const fs = require('fs');
+var i;
+var res;
 
- 
+i = 0;
+res = 0;
+const fs = require('fs');
+fs.readFile(process.argv[2], function(err, data){
+	while(data[i])
+	{
+		if (data[i] == '\n')
+			res++
+		i++
+	}
+})
+
+res++
+console.log(res)
