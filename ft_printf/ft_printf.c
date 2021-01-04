9d4aab3972d3f70525b94142c19d1781bccca16c
HinataKikuchi <haiao4041@gmail.com>
2021-01-05 00:05:53 +0900

    added samary

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: hkikuchi <hkikuchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 14:29:16 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/01/04 23:45:42 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/01/04 23:55:00 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int     ft_printf(const char *, ...)
+int     ft_printf(const char *arg, ...)
 {
     va_list ap;
 
