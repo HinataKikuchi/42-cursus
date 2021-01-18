570025e5d4ddd97ea72ec0d9f2701c0f1b9878e6
HinataKikuchi <haiao4041@gmail.com>
2021-01-18 16:22:24 +0900

    added ft_printf.c function to store formats

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,32 +6,53 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 14:29:16 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/01/18 15:49:16 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/01/18 16:21:31 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "./libft/libft.h"
 #include "./libft/ft_strlen.c"
+#include "./libft/ft_substr.c"
+#include "./libft/ft_calloc.c"
+#include "./libft/ft_atoi.c"
+#include "./libft/ft_bzero.c"
+#include "./libft/ft_isdigit.c"
+
+/*
+** int		ft_printf(const char *format, ...)
+** write characters till '%' appears
+*/
 
 int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
-	// char		*s;
 	int			i;
 
 	i = 0;
 	va_start(ap, format);
-	// s = va_arg(ap, char*);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-			deal_format(format, ap, i);
+		{
+			deal_format(format, ap, i + 1);
+			
+		}
 		write(1, &format[i], 1);
 		i++;
 	}
 	return(i);
 }
+
+void	write_target(const char *target, va_list ap, s_format x)
+{
+	
+}
+
+/*
+**void deal_format(const char *format, va_list ap, int i)
+** save the formats to s_format
+*/
 
 void deal_format(const char *format, va_list ap, int i)
 {
@@ -43,11 +64,17 @@ void deal_format(const char *format, va_list ap, int i)
 	{
 		if (format[i + j] == '-' || (format[i + j] == '0' ||format[i + j] == '*'))
 			x.flagment[j] = format[i + j];
-		
+		if (format[i + j] == '.')
+			x.min_field = ft_atoi(ft_substr(format,i + ft_strlen(x.flagment), j));
 		j++;
 	}
 	x.format_char = format[i + j];
 }
+
+/*
+** int		judge_format(char c)
+** judge the format of c
+*/
 
 int		judge_format(char c)
 {
@@ -55,10 +82,11 @@ int		judge_format(char c)
 
 	if ((c == 'c' || c =='s') || (c == 'p' || c == 'd'))
 		return (0);
-	else if((c =='i' || c =='u') || (c =='i' || c =='u'))
+	else if((c == 'i' || c =='u') || (c =='i' || c =='u'))
 		return (0);
-	else if (c =='x' || c =='X')
+	else if (c == 'x' || c == 'X')
 		return (0);
+	else if (c == '%')
 	flag.format_char = c;
 	return (1);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 14:12:29 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/01/18 15:33:57 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/01/18 16:09:37 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ typedef struct	t_format
 	char	format_char;
 }				s_format;
 
-int		ft_printf(const char *, ...);
+int		ft_printf(const char *format, ...);
 int		judge_format(char c);
 void	deal_format(const char *target, va_list ap, int i);
-
+void	write_target(const char *target, va_list ap, s_format x);
 
 #endif
 
