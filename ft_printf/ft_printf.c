73480254a3de915465dabab8474483dd0fbea995
HinataKikuchi <haiao4041@gmail.com>
2021-01-23 00:52:24 +0900

    added ft_printf.c 's deal_format function

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,21 +6,21 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 14:29:16 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/01/22 16:58:11 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/01/22 20:35:43 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "./libft/ft_strlen.c"
-#include "./libft/ft_substr.c"
-#include "./libft/ft_calloc.c"
-#include "./libft/ft_atoi.c"
-#include "./libft/ft_bzero.c"
-#include "./libft/ft_isdigit.c"
-#include "./libft/ft_strchr.c"
-#include "get_value.c"
-#include "ft_write_char.c"
-#include "ft_write_string.c"
+// #include "./libft/ft_strlen.c"
+// #include "./libft/ft_substr.c"
+// #include "./libft/ft_calloc.c"
+// #include "./libft/ft_atoi.c"
+// #include "./libft/ft_bzero.c"
+// #include "./libft/ft_isdigit.c"
+// #include "./libft/ft_strchr.c"
+// #include "get_value.c"
+// #include "ft_write_char.c"
+// #include "ft_write_string.c"
 
 
 /*
@@ -94,6 +94,7 @@ s_format deal_format(const char *format, int *i, va_list ap)
 	j = 0;
 	*i += 1;
 	x.flagment = malloc(3 * sizeof(char));
+	x.flagment[0] = '\0';
 	while (judge_format(format[*i + j]))
 	{
 		if (format[*i + j] == '-' || (format[*i + j] == '0'))
@@ -129,73 +130,73 @@ int		judge_format(char c)
 	return (1);
 }
 
-int main (void)
-{
-	char *s = "Hinata";
-	printf("__STRING__TEST__\n");
-	printf("%%s\n");
-	printf("[%s]\n",s);
-	ft_printf("[%s]\n",s);
-	printf("%%3s\n");
-	printf("[%3s]\n",s);
-	ft_printf("[%3s]\n",s);
-	printf("%%.3s\n");
-	printf("[%.3s]\n",s);
-	ft_printf("[%.3s]\n",s);
-	printf("[%%3.3s]\n");
-	printf("[%3.3s]\n",s);
-	ft_printf("[%3.3s]\n",s);
-	printf("[%%-3s]\n");
-	printf("[%-3s]\n",s);
-	ft_printf("[%-3s]\n",s);
-	printf("[%%-.3s]\n");
-	printf("[%-.3s]\n",s);
-	ft_printf("[%-.3s]\n",s);
-	printf("printf(\"[%%-3.3s]\",s);\n");
-	printf("[%-3.3s]\n",s);
-	ft_printf("[%-3.3s]\n",s);
-	printf("printf(\"[%%03s]\",s);\n");
-	printf("[%03s]\n",s);
-	ft_printf("[%03s]\n",s);
-	printf("printf(\"[%%0.3s]\",s);\n");
-	printf("[%0.3s]\n",s);
-	ft_printf("[%0.3s]\n",s);
-	printf("printf(\"[%%.03s]\",s);\n");
-	printf("[%.03s]\n",s);
-	ft_printf("[%.03s]\n",s);
-	printf("printf(\"[%%03.03s]\",s);\n");
-	printf("[%03.03s]\n",s);
-	ft_printf("[%03.03s]\n",s);
-	printf("\n\n");
-	printf("printf(\"[%%7s]\");\n");
-	printf("[%7s]\n",s);
-	ft_printf("[%7s]\n",s);
-	printf("printf(\"[%%.7s]\",s);\n");
-	printf("[%.7s]\n",s);
-	ft_printf("[%.7s]\n",s);
-	printf("printf(\"[%%7.7s]\",s);)");
-	printf("[%7.7s]\n",s);
-	ft_printf("[%7.7s]\n",s);
-	ft_printf("[%7.7s]\n",s);
-	printf("printf(\"[%%-7s]\",s);\n");
-	printf("[%-7s]\n",s);
-	ft_printf("[%-7s]\n",s);
-	printf("printf(\"[%%-.7s]\",s);\n");
-	printf("[%-.7s]\n",s);
-	ft_printf("[%-.7s]\n",s);
-	printf("printf(\"[%%-7.7s]\",s);\n");
-	printf("[%-7.7s]\n",s);
-	ft_printf("[%-7.7s]\n",s);
-	printf("printf(\"[%%07s]\",s);\n");
-	printf("[%07s]\n",s);
-	ft_printf("[%07s]\n",s);
-	printf("printf(\"[%%0.7s]\",s);\n");
-	printf("[%0.7s]\n",s);
-	ft_printf("[%0.7s]\n",s);
-	printf("printf(\"[%%.07s]\",s);\n");
-	printf("[%.07s]\n",s);
-	ft_printf("[%.07s]\n",s);
-	printf("printf(\"[%%07.07s]\",s);\n");
-	printf("[%07.07s]\n",s);
-	ft_printf("[%07.07s]\n",s);
-}
+// int main (void)
+// {
+// 	char *s = "Hinata";
+// 	printf("__STRING__TEST__\n");
+// 	printf("%%s\n");
+// 	printf("[%s]\n",s);
+// 	ft_printf("[%s]\n",s);
+// 	printf("%%3s\n");
+// 	printf("[%3s]\n",s);
+// 	ft_printf("[%3s]\n",s);
+// 	printf("%%.3s\n");
+// 	printf("[%.3s]\n",s);
+// 	ft_printf("[%.3s]\n",s);
+// 	printf("[%%3.3s]\n");
+// 	printf("[%3.3s]\n",s);
+// 	ft_printf("[%3.3s]\n",s);
+// 	printf("[%%-3s]\n");
+// 	printf("[%-3s]\n",s);
+// 	ft_printf("[%-3s]\n",s);
+// 	printf("[%%-.3s]\n");
+// 	printf("[%-.3s]\n",s);
+// 	ft_printf("[%-.3s]\n",s);
+// 	printf("printf(\"[%%-3.3s]\",s);\n");
+// 	printf("[%-3.3s]\n",s);
+// 	ft_printf("[%-3.3s]\n",s);
+// 	printf("printf(\"[%%03s]\",s);\n");
+// 	printf("[%03s]\n",s);
+// 	ft_printf("[%03s]\n",s);
+// 	printf("printf(\"[%%0.3s]\",s);\n");
+// 	printf("[%0.3s]\n",s);
+// 	ft_printf("[%0.3s]\n",s);
+// 	printf("printf(\"[%%.03s]\",s);\n");
+// 	printf("[%.03s]\n",s);
+// 	ft_printf("[%.03s]\n",s);
+// 	printf("printf(\"[%%03.03s]\",s);\n");
+// 	printf("[%03.03s]\n",s);
+// 	ft_printf("[%03.03s]\n",s);
+// 	printf("\n\n");
+// 	printf("printf(\"[%%7s]\");\n");
+// 	printf("[%7s]\n",s);
+// 	ft_printf("[%7s]\n",s);
+// 	printf("printf(\"[%%.7s]\",s);\n");
+// 	printf("[%.7s]\n",s);
+// 	ft_printf("[%.7s]\n",s);
+// 	printf("printf(\"[%%7.7s]\",s);)");
+// 	printf("[%7.7s]\n",s);
+// 	ft_printf("[%7.7s]\n",s);
+// 	ft_printf("[%7.7s]\n",s);
+// 	printf("printf(\"[%%-7s]\",s);\n");
+// 	printf("[%-7s]\n",s);
+// 	ft_printf("[%-7s]\n",s);
+// 	printf("printf(\"[%%-.7s]\",s);\n");
+// 	printf("[%-.7s]\n",s);
+// 	ft_printf("[%-.7s]\n",s);
+// 	printf("printf(\"[%%-7.7s]\",s);\n");
+// 	printf("[%-7.7s]\n",s);
+// 	ft_printf("[%-7.7s]\n",s);
+// 	printf("printf(\"[%%07s]\",s);\n");
+// 	printf("[%07s]\n",s);
+// 	ft_printf("[%07s]\n",s);
+// 	printf("printf(\"[%%0.7s]\",s);\n");
+// 	printf("[%0.7s]\n",s);
+// 	ft_printf("[%0.7s]\n",s);
+// 	printf("printf(\"[%%.07s]\",s);\n");
+// 	printf("[%.07s]\n",s);
+// 	ft_printf("[%.07s]\n",s);
+// 	printf("printf(\"[%%07.07s]\",s);\n");
+// 	printf("[%07.07s]\n",s);
+// 	ft_printf("[%07.07s]\n",s);
+// }
