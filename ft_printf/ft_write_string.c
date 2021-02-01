fe0c204e113e165120fcf65464aa5a3c3ccc2f00
HinataKikuchi <haiao4041@gmail.com>
2021-02-01 15:29:15 +0900

    fixed with null string

--- ft_printf/ft_write_string.c
+++ ft_printf/ft_write_string.c
@@ -6,22 +6,36 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:24:24 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/01/31 18:45:14 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/01 15:28:38 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	write_string_with_ac(char *s, s_format x)
+static char	*cut_string(char *s, s_format x)
 {
-	int		i;
+	char *res;
 
-	i = 0;
-	while (i < x.ac)
+	res = calloc((x.ac + 1), sizeof(char));
+	ft_strlcpy(res, s, x.ac + 1);
+	res[x.ac] = '\0';
+	free(s);
+	return (res);
+}
+
+static void	write_s_with_min(char *s, s_format x)
+{
+	if (ft_strchr(x.flagment, '-'))
 	{
-		write(1, &s[i], 1);
-		i++;
+		write(1, s, ft_strlen(s));
+		write_blank(x.min - (int)ft_strlen(s));
+		return;
 	}
+	else if (ft_strchr(x.flagment, '0'))
+		write_zero(x.min - (int)ft_strlen(s));
+	else
+		write_blank(x.min - (int)ft_strlen(s));
+	write(1, s, ft_strlen(s));
 }
 
 void		write_string(va_list ap, s_format x)
@@ -29,26 +43,18 @@ void		write_string(va_list ap, s_format x)
 	char	*s;
 
 	s = va_arg(ap, char*);
-	if (!s)
-		s = "(null)";
+	s = (!s) ? ft_strdup("(null)") : ft_strdup(s);
 	get_min_field(&x);
-	if ((size_t)x.ac < ft_strlen(s) && x.ac)
-		write_string_with_ac(s, x);
-	else if(x.min)
+	if (!x.ac && !x.min)
 	{
-		if (ft_strchr(x.flagment, '-'))
-		{
-			write(1, s, ft_strlen(s));
-			write_blank(x.min - (int)ft_strlen(s));
-			return ;
-		}
-		else if (ft_strchr(x.flagment, '0'))
-			write_zero(x.min - (int)ft_strlen(s));
-		else
-			write_blank(x.min - (int)ft_strlen(s));
-		write(1, s, ft_strlen(s));
+		free(s);
+		return ;
 	}
+	if ((size_t)x.ac < ft_strlen(s) && x.ac)
+		s = cut_string(s, x);
+	if (ft_strlen(s) < (size_t)x.min)
+		write_s_with_min(s, x);
 	else
 		write(1, s, ft_strlen(s));
+	free(s);
 }
-
