2f5d158a1c81a9b6c78a88ce38903d74a846d0e3
HinataKikuchi <haiao4041@gmail.com>
2021-02-02 17:23:44 +0900

    add conma if

--- ft_printf/ft_write_digit.c
+++ ft_printf/ft_write_digit.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:25:05 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/02/02 16:40:45 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/02 17:09:34 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,8 @@ void	write_digit(va_list ap, s_format x)
 	char	*d_s;
 
 	get_min_field(ap,&x);
-	if (ft_strcmp(x.format_num,"0.0") == 0 || ((d = va_arg(ap, int)) == 0 && !x.min && !x.ac))
+	if (ft_strcmp(x.format_num,"0.0") == 0 || \
+	((d = va_arg(ap, int)) == 0 && !x.min && !x.ac && ft_strchr(x.format_num,'.')))
 		return ;
 	d_s = ft_itoa(d);
 	if (ft_strlen(d_s) < (size_t)x.ac)
