b85f7e0c24531db45dd7e2ef14cf2b4735bcbd97
HinataKikuchi <haiao4041@gmail.com>
2021-02-02 16:48:59 +0900

    /home/hkikuchi/.vscode-server/extensions/vadimcn.vscode-lldb-1.6.1/adapter/codelldb terminal-agent --port=41959

--- ft_printf/ft_write_digit.c
+++ ft_printf/ft_write_digit.c
@@ -6,11 +6,32 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:25:05 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/02/02 15:47:41 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/02 16:40:45 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned char	*s1_t;
+	unsigned char	*s2_t;
+
+	i = 0;
+	s1_t = (unsigned char*)s1;
+	s2_t = (unsigned char*)s2;
+	while (*s1_t != '\0' || *s2_t != '\0')
+	{
+		if (*s1_t != *s2_t)
+			return (*s1_t - *s2_t);
+		s1_t++;
+		s2_t++;
+		i++;
+	}
+	return (0);
+}
+
 
 void	write_digit(va_list ap, s_format x)
 {
@@ -18,9 +39,8 @@ void	write_digit(va_list ap, s_format x)
 	char	*d_s;
 
 	get_min_field(ap,&x);
-	if (!x.min && !x.ac && ft_strchr(x.format_num, '.'))
+	if (ft_strcmp(x.format_num,"0.0") == 0 || ((d = va_arg(ap, int)) == 0 && !x.min && !x.ac))
 		return ;
-	d = va_arg(ap, int);
 	d_s = ft_itoa(d);
 	if (ft_strlen(d_s) < (size_t)x.ac)
 	{
