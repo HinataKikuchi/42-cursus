ce6b838c170bfa8575ce0beadb6ecd5132bf82b7
HinataKikuchi <haiao4041@gmail.com>
2021-02-02 17:48:15 +0900

    treated some negative digit

--- ft_printf/make_string.c
+++ ft_printf/make_string.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 17:36:48 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/02/02 17:40:28 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/02 17:46:45 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,17 +50,17 @@ char	*make_dstring(char *d_s, s_format x, int d)
 
 	if (d < 0)
 	{
-		buf = malloc(sizeof(char) * (x.ac + 1));
+		buf = malloc(sizeof(char) * (x.ac + 2));
 		if (!buf)
 			return (NULL);
 		buf[0] = '-';
 		free(d_s);
 		d_s = ft_itoa(d * (-1));
 		d_s_len = ft_strlen(d_s);
-		zero_len = (size_t)x.ac - d_s_len;
+		zero_len = (size_t)x.ac - d_s_len + 1;
 		fill_zero(&buf[1], zero_len);
 		ft_strlcpy(&buf[zero_len], d_s, d_s_len + 1);
-		buf[x.ac] = '\0';
+		buf[x.ac + 2] = '\0';
 		free(d_s);
 		return (buf);
 	}
