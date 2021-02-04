df35fd04953c0399a9b9a26c4356bf54cd9caf43
HinataKikuchi <haiao4041@gmail.com>
2021-02-04 17:24:22 +0900

    repaired digit return

--- ft_printf/ft_write_digit.c
+++ ft_printf/ft_write_digit.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:25:05 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/02/04 13:02:59 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/04 17:23:52 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ void	write_digit(va_list ap, t_format *x)
 	get_min_field(ap,x);
 	if (ft_strcmp(x->format_num,"0.0") == 0 || \
 	((d = va_arg(ap, int)) == 0 && !x->min && !x->ac && ft_strchr(x->format_num,'.')))
+	{
+		x->word_count = 0;
 		return ;
+	}
 	d_s = ft_itoa(d);
 	ds_len = ft_strlen(d_s);
-	if (((d > 0) && ds_len < (size_t)x->ac) || ((d < 0) && ds_len - 1 < (size_t)x->ac))
+	if (((d >= 0) && ds_len < (size_t)x->ac) || ((d < 0) && ds_len - 1 < (size_t)x->ac))
 	{
 		d_s = make_dstring(d_s, *x, d);
 		if (!d_s)
