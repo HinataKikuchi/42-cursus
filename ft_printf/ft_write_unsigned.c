6c0184026370e007b210f0b3402b60223df753b3
HinataKikuchi <haiao4041@gmail.com>
2021-02-08 22:17:24 +0900

    added for unsigned digit but I want to sleep. would you like to sleep with me?

--- ft_printf/ft_write_unsigned.c
+++ ft_printf/ft_write_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: hkikuchi <hkikuchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 00:02:48 by hkikuchi          #+#    #+#             */
-/*   Updated: 2021/02/04 18:22:58 by hkikuchi         ###   ########.fr       */
+/*   Updated: 2021/02/08 22:16:54 by hkikuchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,24 +62,48 @@ void	write_unsigned(va_list ap, t_format *x)
 {
 	unsigned int	ud;
 	char			*ud_s;
+	size_t	ds_len;
 
-	get_min_field(ap,x);
-	if (ft_strcmp(x->format_num,"0.0") == 0 || \
-	((ud = va_arg(ap, unsigned int)) == 0 && !x->min && !x->ac && ft_strchr(x->format_num,'.')))
+	ud = va_arg(ap, unsigned int);
+	if (!ud && !x->min && !x->ac && ft_strchr(x->format_num, '.'))
 	{
 		x->word_count = 0;
 		return ;
 	}
-	ud_s = ft_unsigned_itoa(ud);
-	if (ft_strlen(ud_s) < (size_t)x->ac)
+	ud_s = (x->ac == 0 && ud == 0 && ft_strchr(x->format_num, '.'))\
+		? ft_strdup("") : ft_itoa(ud);
+	ds_len = ft_strlen(ud_s);
+	if (x->minus_flag)
 	{
-		ud_s = make_string(ud_s, *x);
-		if (!ud_s)
-			return ;
+		if (x->ac > ds_len)
+			x->word_count += write_zero(x->ac - ds_len);
+		x->word_count += write(1, ud_s, ds_len);
+		if (x->min > x->word_count)
+			x->word_count += write_blank(x->min - x->word_count);
 	}
-	if (ft_strlen(ud_s) < (size_t)x->min)
-		x->word_count = write_with_min(ud_s, *x);
 	else
-		x->word_count = write(1, ud_s, ft_strlen(ud_s));
-	free(ud_s);
+	{
+
+		x->word_count = write_ud(ud, ud_s, *x, ds_len);
+	}
+	safe_free(ud_s);
+
+	// if (ft_strcmp(x->format_num,"0.0") == 0 || \
+	// ((ud = va_arg(ap, unsigned int)) == 0 && !x->min && !x->ac && ft_strchr(x->format_num,'.')))
+	// {
+	// 	x->word_count = 0;
+	// 	return ;
+	// }
+	// ud_s = ft_unsigned_itoa(ud);
+	// if (ft_strlen(ud_s) < (size_t)x->ac)
+	// {
+	// 	ud_s = make_string(ud_s, *x);
+	// 	if (!ud_s)
+	// 		return ;
+	// }
+	// if (ft_strlen(ud_s) < (size_t)x->min)
+	// 	x->word_count = write_with_min(ud_s, *x);
+	// else
+	// 	x->word_count = write(1, ud_s, ft_strlen(ud_s));
+	// free(ud_s);
 }
