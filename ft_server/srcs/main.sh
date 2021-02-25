822849511c2c45a2298fd24e476f874488236dc2
HinataKikuchi <haiao4041@gmail.com>
2021-02-25 03:30:04 +0100

    done with http

--- ft_server/srcs/main.sh
+++ ft_server/srcs/main.sh
@@ -2,10 +2,7 @@
 
 service nginx start
 service mysql start
-#mysql -e "create database wp_db;"
-#mysql -e "create user 'wpuser'@'localhost' indentified by 'P@ssw0rd';"
-#mysql -e "grant all on wp_db.* to 'wpuser'@'localhost';"
-#mysql -e "FLUSH PRIVILEGES;"
 service php7.3-fpm start
+chown www-data.www-data /var/www/html/ -R
 tail -f /dev/null
 
