c23216f58f5058631ac4755b431c57c107114b2a
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-04-09 18:43:02 +0900

    yeahga .

--- cub3d/test.c
+++ cub3d/test.c
@@ -5,6 +5,16 @@ int	main(void)
 {
 	void *mlxptr;
 	mlxptr = mlx_init();
-	mlx_new_window(mlxptr, 500, 500, "Hello World!");
+	void *winptr;
+	winptr = mlx_new_window(mlxptr, 500, 500, "Hello World!");
+	int i;
+	int j;
+	for (i = 0; i < 400; i++)
+	{
+		for (j = 0;j < 400; j ++)
+		{
+			mlx_pixel_put(mlxptr, winptr, i, j, 4041);
+		}
+	}
 	mlx_loop(mlxptr);
 }
