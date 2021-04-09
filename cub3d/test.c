c8ecab829c2e8e4d9a4b163eeb42487de3cfe1f4
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-04-09 18:34:13 +0900

    add

--- cub3d/test.c
+++ cub3d/test.c
@@ -1,20 +1,10 @@
 #include "./minilibx-linux/mlx.h"
-<<<<<<< HEAD
 #include <stdlib.h>
-=======
-#include <stdio.h>
->>>>>>> 55d043525254b9ded2c23646f7810e6eeec0ea6a
 
 int	main(void)
 {
 	void *mlxptr;
 	mlxptr = mlx_init();
-<<<<<<< HEAD
-	mlx_new_window(mlxptr, 1920, 1080, "Hello World!");
+	mlx_new_window(mlxptr, 500, 500, "Hello World!");
 	mlx_loop(mlxptr);
 }
-=======
-	mlx_new_window(mlxptr, 500, 500, "ababa");
-	mlx_loop(mlxptr);
-}
->>>>>>> 55d043525254b9ded2c23646f7810e6eeec0ea6a
