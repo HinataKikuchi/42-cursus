7c132125e4ddaa6c13d645c419f3ae19986acfc1
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-05-16 20:34:22 +0900

    cutted key funcs

--- new_cub3d/srcs/cub_key.c
+++ new_cub3d/srcs/cub_key.c
@@ -1,44 +1,52 @@
 #include "../include/cub3d.h"
 
+static void	key_Up(t_pos *pos)
+{
+	if (pos->cub.map[(int)(pos->posX + pos->dirX * pos->moveSpeed)][(int)pos->posY] == '0')
+		pos->posX += pos->dirX * pos->moveSpeed;
+	if (pos->cub.map[(int)pos->posX][(int)(pos->posY + pos->dirY * pos->moveSpeed)] == '0')
+		pos->posY += pos->dirY * pos->moveSpeed;
+}
+
+static void	key_Down(t_pos *pos)
+{
+	if (pos->cub.map[(int)(pos->posX - pos->dirX * pos->moveSpeed)][(int)(pos->posY)] == '0')
+		pos->posX -= pos->dirX * pos->moveSpeed;
+	if (pos->cub.map[(int)pos->posX][(int)(pos->posX - pos->dirX * pos->moveSpeed)] == '0')
+		pos->posY -= pos->dirY * pos->moveSpeed;
+}
+
+static void	key_Right(t_pos *pos)
+{
+	double oldDirX = pos->dirX;
+	double oldplaneX = pos->planeX;
+	/*Rotate matrix*/
+	pos->dirX = pos->dirX * cos(-pos->rotSpeed) - pos->dirY * sin(-pos->rotSpeed);
+	pos->dirY = oldDirX * sin(-pos->rotSpeed) + pos->dirY * cos(-pos->rotSpeed);
+	pos->planeX = pos->planeX * cos(-pos->rotSpeed) - pos->planeY * sin(-pos->rotSpeed);
+	pos->planeX = oldplaneX * sin(-pos->rotSpeed) + pos->planeY * cos(-pos->rotSpeed);
+}
+
+static void	key_Left(t_pos *pos)
+{
+	double oldDirX = pos->dirX;
+	double oldplaneX = pos->planeX;
+	pos->dirX = pos->dirX * cos(pos->rotSpeed) - pos->dirY * sin(pos->rotSpeed);
+	pos->dirY = oldDirX * sin(pos->rotSpeed) + pos->dirY * cos(pos->rotSpeed);
+	pos->planeX = pos->planeX * cos(pos->rotSpeed) - pos->planeY * sin(pos->rotSpeed);
+	pos->planeY = oldplaneX * sin(pos->rotSpeed) + pos->planeY * sin(pos->rotSpeed);
+}
 
 int		key_press(int key_code, t_pos *pos)
 {
 	if (key_code == UP || key_code == W)
-	{
-		// if (pos->posX < pos->cub.map_row && pos->posY < 11 /*pos->cub.map_col*/)
-		// {
-			if (pos->cub.map[(int)(pos->posX + pos->dirX * pos->moveSpeed)][(int)pos->posY] == '0')
-				pos->posX += pos->dirX * pos->moveSpeed;
-			if (pos->cub.map[(int)pos->posX][(int)(pos->posY + pos->dirY * pos->moveSpeed)] == '0')
-				pos->posY += pos->dirY * pos->moveSpeed;
-		// }
-	}
+		key_Up(pos);
 	if (key_code == DOWN || key_code == S)
-	{
-		if (pos->cub.map[(int)(pos->posX - pos->dirX * pos->moveSpeed)][(int)(pos->posY)] == '0')
-			pos->posX -= pos->dirX * pos->moveSpeed;
-		if (pos->cub.map[(int)pos->posX][(int)(pos->posX - pos->dirX * pos->moveSpeed)] == '0')
-			pos->posY -= pos->dirY * pos->moveSpeed;
-	}
+		key_Down(pos);
 	if (key_code == RIGHT || key_code == D)
-	{
-		double oldDirX = pos->dirX;
-		double oldplaneX = pos->planeX;
-		/*Rotate matrix*/
-		pos->dirX = pos->dirX * cos(-pos->rotSpeed) - pos->dirY * sin(-pos->rotSpeed);
-		pos->dirY = oldDirX * sin(-pos->rotSpeed) + pos->dirY * cos(-pos->rotSpeed);
-		pos->planeX = pos->planeX * cos(-pos->rotSpeed) - pos->planeY * sin(-pos->rotSpeed);
-		pos->planeX = oldplaneX * sin(-pos->rotSpeed) + pos->planeY *cos(-pos->rotSpeed);
-	}
+		key_Right(pos);
 	if (key_code == LEFT || key_code == A)
-	{
-		double oldDirX = pos->dirX;
-		double oldplaneX = pos->planeX;
-		pos->dirX = pos->dirX * cos(pos->rotSpeed) - pos->dirY * sin(pos->rotSpeed);
-		pos->dirY = oldDirX * sin(pos->rotSpeed) + pos->dirY * cos(pos->rotSpeed);
-		pos->planeX = pos->planeX * cos(pos->rotSpeed) - pos->planeY * sin(pos->rotSpeed);
-		pos->planeY = oldplaneX * sin(pos->rotSpeed) + pos->planeY * sin(pos->rotSpeed);
-	}
+		key_Left(pos);
 	if (key_code == ESC)
 		exit(0);
 	return (0);
